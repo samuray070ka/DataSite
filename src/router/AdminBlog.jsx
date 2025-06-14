224f660963ee371914fda2da4003bee6f5a2130f
Muhammadsodiq <markelofmuhammadsodiq@gmail.com>
2025-06-14 11:13:14 +0500

    all clear

--- src/router/AdminBlog.jsx
+++ src/router/AdminBlog.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './admin.css';
 
@@ -8,7 +8,6 @@ function AdminBlog() {
     description: '',
     videoUrl: ''
   });
-
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -20,58 +19,43 @@ function AdminBlog() {
       await axios.post('http://localhost:5000/api/blogs', formData);
       alert('Blog muvaffaqiyatli qo‘shildi!');
       setFormData({ title: '', description: '', videoUrl: '' });
-      fetchBlogs();
     } catch (error) {
       alert('Xatolik yuz berdi: ' + error.message);
     }
   };
 
-  const fetchBlogs = async () => {
-    try {
-      const res = await axios.get('http://localhost:5000/api/blogs');
-      setBlogs(res.data);
-    } catch (error) {
-      console.log('Xatolik:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchBlogs();
-  }, []);
-
   return (
     <div className='admin'>
-
-    <div className="admin-container">
-      <h2>Blog qo‘shish</h2>
-      <form onSubmit={handleSubmit} className="admin-form">
-        <input
-          type="text"
-          name="title"
-          placeholder="Sarlavha"
-          value={formData.title}
-          onChange={handleChange}
-          required
+      <div className="admin-container">
+        <h2>Blog qo‘shish</h2>
+        <form onSubmit={handleSubmit} className="admin-form">
+          <input
+            type="text"
+            name="title"
+            placeholder="Sarlavha"
+            value={formData.title}
+            onChange={handleChange}
+            required
           />
-        <textarea
-          name="description"
-          placeholder="Tavsif"
-          value={formData.description}
-          onChange={handleChange}
-          required
+          <textarea
+            name="description"
+            placeholder="Tavsif"
+            value={formData.description}
+            onChange={handleChange}
+            required
           ></textarea>
-        <input
-          type="text"
-          name="videoUrl"
-          placeholder="YouTube video URL"
-          value={formData.videoUrl}
-          onChange={handleChange}
-          required
+          <input
+            type="text"
+            name="videoUrl"
+            placeholder="YouTube video URL"
+            value={formData.videoUrl}
+            onChange={handleChange}
+            required
           />
-        <button type="submit">Qo‘shish</button>
-      </form>
+          <button type="submit">Qo‘shish</button>
+        </form>
+      </div>
     </div>
-          </div>
   );
 }
 
