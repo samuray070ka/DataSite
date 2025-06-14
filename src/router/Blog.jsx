36b30275b127d185a688a703e29a7158992d691c
Muhammadsodiq <markelofmuhammadsodiq@gmail.com>
2025-06-14 14:30:54 +0500

    clera

--- src/router/Blog.jsx
+++ src/router/Blog.jsx
@@ -11,42 +11,46 @@ function Blog() {
       .catch(err => console.error("Xatolik:", err));
   }, []);
 
- const getEmbedUrl = (url) => {
-  try {
-    if (url.includes('watch?v=')) {
-      return url.replace('watch?v=', 'embed/');
-    } else if (url.includes('youtu.be/')) {
-      const id = url.split('youtu.be/')[1];
-      return `https://www.youtube.com/embed/${id}`;
-    } else if (url.includes('/shorts/')) {
-      const id = url.split('/shorts/')[1];
-      return `https://www.youtube.com/embed/${id}`;
-    } else {
-      return url;
+  const getEmbedUrl = (url) => {
+    try {
+      if (url.includes('watch?v=')) {
+        return url.replace('watch?v=', 'embed/');
+      } else if (url.includes('youtu.be/')) {
+        const id = url.split('youtu.be/')[1];
+        return `https://www.youtube.com/embed/${id}`;
+      } else if (url.includes('/shorts/')) {
+        const id = url.split('/shorts/')[1];
+        return `https://www.youtube.com/embed/${id}`;
+      } else {
+        return url;
+      }
+    } catch {
+      return '';
     }
-  } catch {
-    return '';
-  }
-};
+  };
 
   return (
     <div className="container blog-container">
-      <h1 className='blog_span'>Bloglar sahifasi</h1>
+      <h1 className='blog-title'>Bloglar sahifasi</h1>
       <div className="blog-grid">
         {blogs.map((item, index) => {
           const embedUrl = getEmbedUrl(item.videoUrl);
+
           return (
             <div className="blog-card" key={index}>
+              {embedUrl && (
                 <iframe
-                //   width="50%"
-                //   height="100"
+                  width="100%"
+                  height="300"
                   src={embedUrl}
-                  title="YouTube video"
+                  title={item.title || "YouTube video"}
                   frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
-                  ></iframe>
-                  <h2>{item.title}</h2>
-                  <p>{item.description}</p>
+                />
+              )}
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
             </div>
           );
         })}
